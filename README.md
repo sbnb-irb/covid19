ca04a0ca0470737d41901565e6e7397b01802db4
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-03-24 13:06:33 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # covid19
 
-ChemicalChecker based candidates for COVID19
+ChemicalChecker based candidates for COVID19.
+
+Tables provided:
+
+## Literature table
+1. mol inchikey (link to CC)
+2. mol name
+3. evidence score
+4. description (wrapped)
+5. aggregated reference link
+
+## Candidates table
+1. mol inchikey
+2. mol name
+3. mol categories (multiple choice)
+4. score (nr similar)
+5. score (nr similar different MoA)
+6. more scores
---
+++ web/app.py
@@ -0,0 +1,87 @@
+import json
+import pandas as pd
+from flask import Flask, request, render_template
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+app = Flask(__name__)
+app.debug = True
+
+def _get_google_table():
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        'covid19_repo.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('Drugs against SARS-CoV-2').get_worksheet(1)
+    records = sheet.get_all_records()
+
+    df = pd.DataFrame(records)
+    return df
+
+
+class BaseDataTables:
+
+    def __init__(self, request, columns, collection):
+        self.columns = columns
+        self.collection = collection
+        # values specified by the datatable for filtering, sorting, paging
+        self.request_values = request.values
+        # results from the db
+        self.result_data = None
+        # total in the table after filtering
+        self.cardinality_filtered = 0
+        # total in the table unfiltered
+        self.cadinality = 0
+        self.run_queries()
+
+    def output_result(self):
+        output = {}
+        # output['sEcho'] = str(int(self.request_values['sEcho']))
+        # output['iTotalRecords'] = str(self.cardinality)
+        # output['iTotalDisplayRecords'] = str(self.cardinality_filtered)
+        aaData_rows = []
+        for row in self.result_data:
+            aaData_row = []
+            for i in range(len(self.columns)):
+                print row, self.columns, self.columns[i]
+                aaData_row.append(
+                    str(row[self.columns[i]]).replace('"', '\\"'))
+            aaData_rows.append(aaData_row)
+        output['aaData'] = aaData_rows
+        return output
+
+    def run_queries(self):
+        self.result_data = self.collection
+        self.cardinality_filtered = len(self.result_data)
+        self.cardinality = len(self.result_data)
+
+
+@app.route('/')
+def index():
+    df = _get_google_table()
+    return render_template('index.html', columns=df.columns)
+    return 'Hello World!'
+
+
+@app.route('/_server_data')
+def get_server_data():
+
+    df = _get_google_table()
+    df['Score'].fillna(-1, inplace=True)
+
+    def make_href(val):
+        return '<a href="{}">link</a>'.format(val, val)
+
+    df['Reference'] = df['Reference'].apply(make_href)
+    columns = df.columns
+    collection = [r.to_dict() for _, r in df.iterrows()]
+
+    results = BaseDataTables(request, columns, collection).output_result()
+    # return the results as a string for the datatable
+    return json.dumps(results)
+
+
+if __name__ == '__main__':
+    app.run()
