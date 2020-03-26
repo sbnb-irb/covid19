0918b27fd84082cdf8235cb3456d5a6fab8d33ef
mduran <miquel.duran@irbbarcelona.org>
2020-03-26 02:05:25 +0100

    resolved merging

--- scripts/similarity_search.py
+++ scripts/similarity_search.py
@@ -13,109 +13,232 @@ from chemicalchecker.core.signature_data import DataSignature
 script_path = os.path.dirname(os.path.realpath(__file__))
 data_path = os.path.join(script_path, '..', 'web', 'data')
 
-df_lit = pd.read_csv(os.path.join(data_path, 'literature.csv'))
+def main(simtype):
 
-# Get vectors
-cc = ChemicalChecker("/aloy/web_checker/package_cc/paper/")
-s3 = cc.signature("ZZ.004", "sign3")
-iks_lit, V_lit = s3.get_vectors(list(df_lit["inchikey"]))
-scores = {}
-for r in df_lit[["inchikey", "score"]].values:
-    scores[r[0]] = r[1]
-scs_lit = np.array([scores[k] for k in iks_lit])
-iks_can, V_can = s3.keys, s3[:]
-scs_can = []
-for k in iks_can:
-    if k in scores:
-        scs_can += [scores[k]]
+    # Load CC signatures
+    cc = ChemicalChecker("/aloy/web_checker/package_cc/paper/")
+    if simtype == "cc":
+        print("Loading CC signatures")
+        neig = cc.signature("ZZ.004", "neig3")
     else:
-        scs_can += [-2]
-scs_can = np.array(scs_can)
+        print("Loading Morgan fingerprints")
+        neig = cc.signature("A1.001", "neig1")
+    
+    universe = set(neig.row_keys)
+    universe_conn = dict([(k.split("-")[0], k) for k in neig.row_keys])
 
-# Metadata
-isdrug_can = np.array([1] * len(iks_can))  # provisional
-nam_can = iks_can  # provisional
-nam_lit = iks_lit  # provisional
-
-# Compute similarities
-dist_euc = cdist(V_can, V_lit, metric="euclidean")
-dist_cos = cdist(V_can, V_lit, metric="cosine")
-
-
-def tautize(V):
-    W = np.zeros(V.shape)
-    for j in range(0, V.shape[1]):
-        ranks = rankdata(-V[:, j], method="ordinal")
-        W[:, j] = ranks / np.max(ranks)
-    return W
-
-
-tau_euc = tautize(dist_euc)
-tau_cos = tautize(dist_cos)
-tau = (tau_euc + tau_cos) / 2
-dist_file =  os.path.join(data_path, 'dist.h5')
-with h5py.File(dist_file, "w") as hf:
-    hf.create_dataset("euc", data=dist_euc)
-    hf.create_dataset("cos", data=dist_cos)
-    hf.create_dataset("tau", data=tau)
-    hf.create_dataset("rows", data=np.array(
-        iks_can, DataSignature.string_dtype()))
-    hf.create_dataset("cols", data=np.array(
-        iks_lit, DataSignature.string_dtype()))
-    hf.create_dataset("scs_rows", data=scs_can)
-    hf.create_dataset("scs_cols", data=scs_lit)
-    hf.create_dataset("isdrug_rows", data=isdrug_can)
-
-# Get matching scores for the candidate molecules
-
-
-def similarities_as_matrix(tau, min_score):
-    if min_score is not None:
-        mask = scs_lit >= min_score
-        tau = tau[:, mask]
-    sim_avg = np.mean(tau, axis=1)
-    sim_q66 = np.percentile(tau, 66, axis=1)
-    cou_95 = np.sum(tau > 0.95, axis=1)
-    cou_99 = np.sum(tau > 0.99, axis=1)
-    cou_999 = np.sum(tau > 0.999, axis=1)
-    max_idx = np.argmax(tau, axis=1)
-    M = np.vstack([sim_avg, sim_q66, cou_95, cou_99, cou_999, max_idx]).T
-    return M
-
-
-def similarities(tau, min_score, sort_by="count_99"):
-    if min_score is not None:
-        if min_score < 0 or min_score > 3:
-            raise Exception("min_score must be None, 0, 1, 2 or 3")
-    M = similarities_as_matrix(tau, min_score)
-    results = {
-        "inchikey": iks_can,
-        "name": nam_can,
-        "is_drug": isdrug_can,
-        "lit_score": scs_can,
-        "sim_avg": M[:, 0],
-        "sim_q66": M[:, 1],
-        "count_95": M[:, 2],
-        "count_99": M[:, 3],
-        "count_999": M[:, 4],
-        "best_inchikey": [iks_lit[int(idx)] for idx in M[:, 5]],
-        "best_name": [nam_lit[int(idx)] for idx in M[:, 5]]
-    }
-    df = pd.DataFrame(results)
-    if min_score is None:
-        fn = "df_cand_all.pkl"
-    else:
-        fn = "df_cand_sc%d.pkl" % min_score
-    fn = os.path.join(data_path, fn)
+    print("Converting inchikeys")
+    # Convert to inchikeys
+    conv = Converter()
+    d = collections.defaultdict(list)
+    for r in df.values:
+        try:
+            mol = conv.smiles_to_inchi(r[2])
+            inchikey, inchi = mol
+            if inchikey not in universe:
+                if inchikey.split("-")[0] not in universe_conn:
+                    continue
+                else:
+                    inchikey = universe_conn[inchikey.split("-")[0]]
+            d[inchikey] += [(r[0], r[1], r[3], r[5], r[4], r[8], r[9], r[10])]
+        except:
+            continue
+    R = []
+    for k, v in d.items():
+        name =  ";".join([x[1] for x in v if type(x[1]) is str])
+        evi = [x[2] for x in v if not np.isnan(x[2])]
+        if len(evi) > 0:
+            evi = np.max(evi)
+        else:
+            evi = -2
+        moa = [x[3] for x in v if not np.isnan(x[3])]
+        if len(moa) > 0:
+            moa = int(np.max(moa))
+        else:
+            moa = -2
+        descriptions = ";".join([x[4] for x in v if type(x[4]) is str])
+        links1 = [x[5] for x in v if type(x[5]) is str]
+        links2 = [x[6] for x in v if type(x[6]) is str]
+        links3 = [x[7] for x in v if type(x[7]) is str]
+        links  = links1 + links2 + links3
+        links  = ";".join(links)
+        R += [(k, name, evi, moa, descriptions, links)]
+    df_lit = pd.DataFrame(R, columns = ["inchikey", "name", "evidence", "moa", "descriptions", "links"])
+    fn = "outputs/df_lit_%s.pkl" % simtype
     with open(fn, "wb") as f:
-        pickle.dump(df, f)
-    return df
+        pickle.dump(df_lit, f)
 
+    print("Getting precomputed similarities")
+    # Get precomputed similarities
+    iks_can = neig.row_keys
+    iks_can_set = set(iks_can)
+    iks_lit = np.array([k for k in sorted(df_lit["inchikey"]) if k in iks_can_set])
+    iks_can_idxs = dict((k,i) for i,k in enumerate(iks_can))
+    idxs_lit = [iks_can_idxs[k] for k in iks_lit]
+    with h5py.File(neig.data_path, "r") as hf:
+        nn = hf["indices"][idxs_lit]
 
-similarities(tau, min_score=None)
-similarities(tau, min_score=0)
-similarities(tau, min_score=1)
-similarities(tau, min_score=2)
-similarities(tau, min_score=3)
+    print("Keeping evidence")
+    evids = {}
+    for r in df_lit[["inchikey", "evidence"]].values:
+        evids[r[0]] = r[1]
+    evi_lit = np.array([evids[k] for k in iks_lit])
+    evi_can = []
+    for k in iks_can:
+        if k in evids:
+            evi_can += [evids[k]]
+        else:
+            evi_can += [-2]
+    evi_can = np.array(evi_can)
+
+    print("Keeping moa")
+    moas = {}
+    for r in df_lit[["inchikey", "moa"]].values:
+        moas[r[0]] = r[1]
+    moa_lit = np.array([moas[k] for k in iks_lit])
+    moa_can = []
+    for k in iks_can:
+        if k in moas:
+            moa_can += [moas[k]]
+        else:
+            moa_can += [-2]
+    moa_can = np.array(moa_can)
+
+    # Metadata
+    print("Metadata: is drug")
+    ## Drugs
+    with open("data/druglist.tsv", "r") as f:
+        druglist = []
+        for l in f:
+            druglist += [l.rstrip("\n")]
+        druglist_conn = [d.split("-")[0] for d in druglist]
+        druglist = set(druglist)
+        druglist_conn = set(druglist_conn)
+    isdrug_can = []
+    for k in iks_can:
+        if k.split("-")[0] in druglist_conn:
+            isdrug_can += [1]
+        else:
+            isdrug_can += [0]
+    isdrug_can = np.array(isdrug_can)
+
+    print("Metadata: names")
+    ## Names
+    ik_name = {}
+    with open("data/inchikeys_names.csv", "r") as f:
+        for l in f:
+            l = l.rstrip("\n")
+            ik = l[:27]
+            name = l[28:]
+            ik_name[ik] = name
+    nam_can = []
+    for k in iks_can:
+        if k in ik_name:
+            nam_can += [ik_name[k]]
+        else:
+            nam_can += [k]
+    #nam_can = np.array(nam_can)
+    nam_lit = []
+    for k in iks_lit:
+        if k in ik_name:
+            nam_lit += [ik_name[k]]
+        else:
+            nam_lit += [k]
+    #nam_lit = np.array(nam_lit)
+    
+    print("Reorganizing ranks")
+    ranks = np.full((len(iks_can),len(iks_lit)),-1).astype(np.int)
+    vals  = [i for i in range(0,nn.shape[1])]
+    for j in range(0, ranks.shape[1]):
+        ranks[nn[j,:],j] = vals
+
+    # Compute similarities
+    with h5py.File("outputs/dist_%s.h5" % simtype, "w") as hf:
+        hf.create_dataset("ranks", data = ranks)
+        hf.create_dataset("rows", data = np.array(iks_can, DataSignature.string_dtype()))
+        hf.create_dataset("cols", data = np.array(iks_lit, DataSignature.string_dtype()))
+        hf.create_dataset("evi_rows", data = evi_can)
+        hf.create_dataset("evi_cols", data = evi_lit)
+        hf.create_dataset("isdrug_rows", data = isdrug_can)
+
+    # Get matching scores for the candidate molecules
+    def similarities_as_matrix(ranks, min_evidence, moa):
+        if min_evidence is None:
+            if moa is None:
+                mask = None
+            else:
+                mask = moa_lit == moa
+        else:
+            mask1 = evi_lit >= min_evidence
+            if moa is None:
+                mask = mask1
+            else:
+                mask2 = moa_lit == moa
+                mask = np.logical_and(mask1, mask2)
+        if not np.any(mask): mask = None
+        if mask is not None:
+            ranks = ranks[:, mask]
+        ranks[ranks == -1] = 99999
+        cou_tp5 = np.sum(ranks <= 4, axis=1).astype(np.int)
+        cou_pv5 = np.sum(ranks <= ranks.shape[0]*1e-5, axis=1).astype(np.int)
+        cou_pv4 = np.sum(ranks <= ranks.shape[0]*1e-4, axis=1).astype(np.int)
+        cou_pv3 = np.sum(ranks <= ranks.shape[0]*1e-3, axis=1).astype(np.int)
+        cou_pv2 = np.sum(ranks <= ranks.shape[0]*1e-2, axis=1).astype(np.int)
+        best_idx = np.argmin(ranks, axis=1).astype(np.int)
+        M = np.vstack([cou_tp5, cou_pv5, cou_pv4, cou_pv3, cou_pv2, best_idx]).astype(np.int)
+        M = M.T
+        return M
+
+    def similarities(simtype, min_evidence, moa, sort_by="lpv_4"):
+        if min_evidence is not None:
+            if min_evidence < 0 or min_evidence > 3:
+                raise Exception("min_evidence must be None, 0, 1, 2 or 3")
+        M = similarities_as_matrix(ranks, min_evidence, moa)
+        results = {
+            "inchikey": iks_can,
+            "name": nam_can,
+            "is_drug": isdrug_can,
+            "evidence": evi_can,
+            "moa": moa_can,
+            "top_5": M[:,0],
+            "lpv_5": M[:,1],
+            "lpv_4": M[:,2],
+            "lpv_3": M[:,3],
+            "lpv_2": M[:,4],
+            "best_inchikey": [iks_lit[int(idx)] for idx in M[:,5]],
+            "best_name": [nam_lit[int(idx)] for idx in M[:,5]]
+        }
+        df = pd.DataFrame(results)
+        print("...filtering")
+        iks = set()
+        for col in ["top_5", "lpv_5", "lpv_4", "lpv_3", "lpv_2"]:
+            df = df.sort_values(col, ascending=False)
+            iks.update(list(df[df[col] > 0]["inchikey"][:10000]))
+        df = df[df["inchikey"].isin(iks)]
+        if min_evidence is None:
+            evi_suf = "eviall"
+        else:
+            evi_suf = "evi%d" % min_evidence
+        if moa is None:
+            moa_suf = "moaall"
+        else:
+            moa_suf = "moa%d" % moa
+        fn = "df_cand_%s_%s_%s.pkl" % (simtype, evi_suf, moa_suf)
+        df = df.sort_values(sort_by, ascending=False)
+        df.to_pickle(os.path.join("outputs", fn))
+        return fn
+
+    print("Saving legend")
+    legend = {}
+    for min_evidence in [None, 0, 1, 2, 3]:
+        for moa in [None, 0, 1, 2, 3, 4, 5]:
+            print("Sim type %s | Min evidence %s | MoA %s" % (simtype, str(min_evidence), str(moa)))
+            legend[(min_evidence, moa)] = similarities(simtype=simtype, min_evidence=min_evidence, moa=moa)
+    with open("outputs/legend_%s.pkl" % simtype, "wb") as f:
+        pickle.dump(legend, f)
+
+if __name__ == "__main__":
+    main(simtype="cc")
+    main(simtype="fp")
 
 print("Done!")
