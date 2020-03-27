9a2c463d83c3b0eddb7232c316674dee1a2489d8
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-03-27 15:42:33 +0100

    load tables upon selection

--- web/app.py
+++ web/app.py
@@ -22,8 +22,17 @@ df_literature = get_literature_data()
 df_literature_collection = df_literature.to_dict(orient='records')
 
 
-def get_candidate_data():
-    file_path = os.path.join(app_path, 'data', 'df_cand_cc_eviall_moaall.csv')
+def get_candidate_data(signature='cc', evidence='', moa=''):
+    if evidence == '':
+        evidence = 'all'
+    else:
+        evidence = int(evidence)
+    if moa == '':
+        moa = 'all'
+    else:
+        moa = int(moa)
+    filen_name = 'df_cand_%s_evi%s_moa%s.csv' % (signature, evidence, moa)
+    file_path = os.path.join(app_path, 'data', filen_name)
     data = pd.read_csv(file_path)
     print('LOADED %s' % file_path)
     print('LENGTH %s' % len(data))
@@ -54,6 +63,14 @@ def get_literature_table():
 
 @app.route('/_candidate_table')
 def get_candidate_table():
+    signature = request.args.get('signature')
+    evidence = request.args.get('evidence')
+    moa = request.args.get('moa')
+    print('signature', signature)
+    print('evidence', evidence)
+    print('moa', moa)
+    df_candidates = get_candidate_data(signature, evidence, moa)
+    df_candidates_collection = df_candidates.to_dict(orient='records')
     columns = list()
     for order, col in enumerate(df_candidates.columns, 1):
         col_dict = {
