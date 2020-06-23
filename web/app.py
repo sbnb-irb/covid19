cd1720459960a9db91cb10be1dd0af7135eff379
Martino Bertoni <martino.bertoni@irbbarcelona.org>
2020-06-23 14:57:22 +0200

    transforming references to url, removed print statements

--- web/app.py
+++ web/app.py
@@ -24,10 +24,10 @@ def get_literature_data():
 
     # link is the column with hyperlinks
     data['References'] = data['References'].apply(urls_to_html)
-    print('LOADED %s' % file_path)
-    print('LENGTH %s' % len(data))
-    print('COLUMNS %s' % str(data.columns))
-    print('EXAMPLE\n%s' % str(data.iloc[0]))
+    #print('LOADED %s' % file_path)
+    #print('LENGTH %s' % len(data))
+    #print('COLUMNS %s' % str(data.columns))
+    #print('EXAMPLE\n%s' % str(data.iloc[0]))
     return data
 
 
@@ -44,10 +44,10 @@ def get_candidate_data(signature='cc', evidence='', moa=''):
     file_path = os.path.join(app_path, 'data', filen_name)
     data = pd.read_csv(file_path, sep="\t")
     data.fillna('!N/A', inplace=True)
-    print('LOADED %s' % file_path)
-    print('LENGTH %s' % len(data))
-    print('COLUMNS %s' % str(data.columns))
-    print('EXAMPLE\n%s' % str(data.iloc[0]))
+    #print('LOADED %s' % file_path)
+    #print('LENGTH %s' % len(data))
+    #print('COLUMNS %s' % str(data.columns))
+    #print('EXAMPLE\n%s' % str(data.iloc[0]))
     return data
 
 
@@ -56,10 +56,10 @@ def get_query_data(query_id, signature):
                              'query_%s_%s.csv' % (query_id, signature))
     data = pd.read_csv(file_path, sep="\t")
     data.fillna('!N/A', inplace=True)
-    print('LOADED %s' % file_path)
-    print('LENGTH %s' % len(data))
-    print('COLUMNS %s' % str(data.columns))
-    print('EXAMPLE\n%s' % str(data.iloc[0]))
+    #print('LOADED %s' % file_path)
+    #print('LENGTH %s' % len(data))
+    #print('COLUMNS %s' % str(data.columns))
+    #print('EXAMPLE\n%s' % str(data.iloc[0]))
     return data
 
 
@@ -106,13 +106,13 @@ def get_literature_table():
     columns = df_literature.columns
     collection = df_literature_collection
     results = BaseDataTables(request, columns, collection).output_result()
-    print("RESULTS", str(results))
+    #print("RESULTS", str(results))
     return jsonify(results)
 
 
 @app.route('/_candidate_table')
 def get_candidate_table():
-    print('get_candidate_table', request.args)
+    #print('get_candidate_table', request.args)
     signature = request.args.get('signature')
     evidence = request.args.get('evidence')
     moa = request.args.get('moa')
@@ -130,13 +130,13 @@ def get_candidate_table():
         columns.append(col_dict)
     collection = df_candidates_collection
     results = ServerSideTable(request, columns, collection).output_result()
-    print("RESULTS", str(results))
+    #print("RESULTS", str(results))
     return jsonify(results)
 
 
 @app.route('/_table_title')
 def get_table_title():
-    print('get_table_title', request.args)
+    #print('get_table_title', request.args)
     signature = request.args.get('signature')
     evidence = request.args.get('evidence')
     moa = request.args.get('moa')
@@ -153,14 +153,14 @@ def get_table_title():
     else:
         moa = float(moa)
         df = df[(df['moa'] == moa)]
-    print('get_table_title RESULT', df)
+    #print('get_table_title RESULT', df)
     assert(len(df) == 1)
     return jsonify(df.iloc[0]['caption'])
 
 
 @app.route('/_table_query')
 def get_query_table():
-    print('get_query_table', request.args)
+    #print('get_query_table', request.args)
     query_id = request.args.get('query')
     signature = request.args.get('signature')
     df_query = get_query_data(query_id, signature)
@@ -177,13 +177,13 @@ def get_query_table():
         columns.append(col_dict)
     collection = df_query_collection
     results = ServerSideTable(request, columns, collection).output_result()
-    print("RESULTS", str(results))
+    #print("RESULTS", str(results))
     return jsonify(results)
 
 
 @app.route('/download')
 def download_file():
-    print('download_file', request.args)
+    #print('download_file', request.args)
     signature = request.args.get('signature')
     evidence = request.args.get('evidence')
     moa = request.args.get('moa')
